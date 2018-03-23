1e73f9ebaa78e01059e75769830f9e246575faa5
carolina.silvag <carolinasguzman@gmail.com>
2018-03-23 08:42:21 -0300

    change config production.rb initialize_on_precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.assets.initialize_on_precompile = false
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
